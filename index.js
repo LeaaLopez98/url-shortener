71bdc602a7d1ad73d6a204d89cc6c4f59d3b9ef7
lea <lea@lea.lea.com>
2025-02-07 11:57:36 -0300

    Update index.js with URL routes

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import './src/config/db.js'
 
+import { urlRouter } from './src/routes/urlRoutes.js'
+
 const app = express()
 
 const PORT = 3000
 
 app.use(express.json())
+app.use('/api/urls', urlRouter)
 
 app.listen(PORT, () => {
   console.info(`Application run in port ${PORT}`)
