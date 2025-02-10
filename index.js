2bd8a719220c68835e63183e00972deed4bbf7d5
lea <lea@lea.lea.com>
2025-02-10 14:09:51 -0300

    Update index.js for auth management

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import './src/config/db.js'
 
 import { urlRouter } from './src/routes/urlRoutes.js'
+import { authRouter } from './src/routes/authRoutes.js'
 
 const app = express()
 
@@ -9,6 +10,7 @@ const PORT = 3000
 
 app.use(express.json())
 app.use('/api/urls', urlRouter)
+app.use('/api/auth', authRouter)
 
 app.listen(PORT, () => {
   console.info(`Application run in port ${PORT}`)
