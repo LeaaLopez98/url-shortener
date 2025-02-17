e4c1e4f0314d09b49b60c1832cd5e682dce2c4a2
lea <lea@lea.lea.com>
2025-02-17 12:41:25 -0300

    Update service to generate short URLs using Redis

--- src/services/generateShortUrl.js
+++ src/services/generateShortUrl.js
@@ -1,13 +1,15 @@
 import Hashids from 'hashids'
+import { getCounterValue } from './counterService.js'
 
 const SALT = 'SOMETHING'
 
 const hashids = new Hashids(SALT, 6)
 
-let counter = 0
+export const generateShortUrl = async () => {
+  const counter = await getCounterValue()
 
-export const generateShortUrl = () => {
-  counter += 1
+  console.log('Counter -> ', counter)
+
   const modifiedCounter = hashids.encode(counter)
 
   return modifiedCounter
